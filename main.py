3fd889e5c152c502a8bc60ab1b68103d8bdae9d5
GouthamNP <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-09-19 20:18:39 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def response_generator(character: str):
 		file = open("responses/darkones.txt", "r")
 		responses = file.readlines()
 		response = random.choice(responses)
-	elif character == "Cottagers":
+	elif character == "De Lacey Family":
 		file = open("responses/cottagers.txt", "r")
 		responses = file.readlines()
 		response = random.choice(responses)
@@ -40,32 +40,66 @@ def response_generator(character: str):
 		time.sleep(0.05)
 
 
+def type_of_character(option):
+	character_text = ""
+	if option == "Victor Frankenstein":
+		character_text = "You are now acting as the Robert Walton, the patient listener of Frankenstein's story."
+	elif option == "His Creature":
+		character_text = "You are now acting as Victor Frankenstein, the agitated listener of his creation."
+	elif option == "De Lacey Family":
+		character_text = (
+			"You are now acting as a passerby, listening to the tale of the interaction between the De Lacey Family and"
+			" the creature.")
+	elif option == "Artyom (from Metro 2033)":
+		character_text = (
+			"You are now acting as a fellow survivor of the global nuclear war, living in the Moscow Metro,"
+			"listening to Artyom opine about the Dark Ones.")
+	elif option == "Dark Ones (mutated humans from Metro 2033)":
+		character_text = ("You are now acting as a human survivor in the Moscow Metro, watching the Dark Ones address "
+		                  "humankind.")
+	elif option == "Royal Family":
+		character_text = (
+			"You are now acting as a royal court member listening to the royal family in Las Meninas talk about "
+			"their beauty.")
+	elif option == "Servants":
+		character_text = (
+			"You are now acting as a family member of the servants of the royal family, listening to your family "
+			"talk about how disadvantageous it is to not be attractive.")
+	else:
+		character_text = "Unknown"
+	for word in character_text.split():
+		yield word + " "
+		time.sleep(0.05)
+
+
 st.title("Literary Chatbot — Frankenstein")
-st.write("Ask different experiences in their own worlds about how society is biased based on appearances")
+st.markdown("###Ask different experiences in their own worlds about how society is biased based on appearances")
+st.markdown("##Characters")
+st.write("1. Victor Frankenstein (Frankenstein)")
+st.write("2. His Creature (Frankenstein)")
+st.write("3. De Lacey Family (Frankenstein)")
+st.write('4. Artyom (protagonist, who is the "chosen one" from Metro 2033)')
+st.write('5. Dark Ones (mutated humans/"antagonists" who get bombed by the survivors of nuclear war | from Metro 2033)')
 
-option = st.selectbox(
-	"Which character would you like to choose? ",
-	("Victor Frankenstein", "His Creature", "Cottagers", "Artyom (from Metro 2033)",
-	 "Dark Ones (mutated humans from Metro 2033)", "Royal Family", "Servants"))
-# Initialize chat history
-if "messages" not in st.session_state:
-	st.session_state.messages = []
+option = st.selectbox("Which character would you like to choose? ",
+                      ("", "Victor Frankenstein", "His Creature", "De Lacey Family", "Artyom (from Metro 2033)",
+                       "Dark Ones (mutated humans from Metro 2033)", "Royal Family", "Servants"))
 
-# Display chat messages from history on app rerun
+st.session_state.messages = []
 for message in st.session_state.messages:
 	with st.chat_message(message["role"]):
 		st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("What would you like to learn?"):
+if option != "":
 	# Add user message to chat history
-	st.session_state.messages.append({"role": "user", "content": prompt})
-	# Display user message in chat message container
-	with st.chat_message("user"):
-		st.markdown(prompt)
-
-	# Display assistant response in chat message container
+	time.sleep(1)
 	with st.chat_message("assistant"):
+		character = st.write_stream(type_of_character(option))
+	# Display user message in chat message container
+	# Display assistant response in chat message container
+	time.sleep(2)
+	with st.chat_message("ai"):
 		response = st.write_stream(response_generator(option))
 	# Add assistant response to chat history
 	st.session_state.messages.append({"role": "assistant", "content": response})
